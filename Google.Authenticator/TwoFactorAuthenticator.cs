e6865a242ae66488ae6155b794c995a090aa2cab
brandonpotter <brandon@x9tech.com>
2015-09-23 11:34:51 -0400

    Fix QR code Google chart URL for IE compatibility, add HTTPS option

--- Google.Authenticator/TwoFactorAuthenticator.cs
+++ Google.Authenticator/TwoFactorAuthenticator.cs
@@ -40,6 +40,21 @@ namespace Google.Authenticator
         /// <returns>SetupCode object</returns>
         public SetupCode GenerateSetupCode(string issuer, string accountTitleNoSpaces, string accountSecretKey, int qrCodeWidth, int qrCodeHeight)
         {
+            return GenerateSetupCode(issuer, accountTitleNoSpaces, accountSecretKey, qrCodeWidth, qrCodeHeight, false);
+        }
+
+        /// <summary>
+        /// Generate a setup code for a Google Authenticator user to scan (with issuer ID).
+        /// </summary>
+        /// <param name="issuer">Issuer ID (the name of the system, i.e. 'MyApp')</param>
+        /// <param name="accountTitleNoSpaces">Account Title (no spaces)</param>
+        /// <param name="accountSecretKey">Account Secret Key</param>
+        /// <param name="qrCodeWidth">QR Code Width</param>
+        /// <param name="qrCodeHeight">QR Code Height</param>
+        /// <param name="useHttps">Use HTTPS instead of HTTP</param>
+        /// <returns>SetupCode object</returns>
+        public SetupCode GenerateSetupCode(string issuer, string accountTitleNoSpaces, string accountSecretKey, int qrCodeWidth, int qrCodeHeight, bool useHttps)
+        {
             if (accountTitleNoSpaces == null) { throw new NullReferenceException("Account Title is null"); }
 
             accountTitleNoSpaces = accountTitleNoSpaces.Replace(" ", "");
@@ -60,7 +75,8 @@ namespace Google.Authenticator
                 provisionUrl = UrlEncode(String.Format("otpauth://totp/{0}?secret={1}&issuer={2}", accountTitleNoSpaces, encodedSecretKey, UrlEncode(issuer)));
             }
 
-            string url = String.Format("http://chart.apis.google.com/chart?cht=qr&chs={0}x{1}&chl={2}", qrCodeWidth, qrCodeHeight, provisionUrl);
+            string protocol = useHttps ? "https" : "http";
+            string url = String.Format("{0}://chart.googleapis.com/chart?cht=qr&chs={1}x{2}&chl={3}", protocol, qrCodeWidth, qrCodeHeight, provisionUrl);
 
             sC.QrCodeSetupImageUrl = url;
 
