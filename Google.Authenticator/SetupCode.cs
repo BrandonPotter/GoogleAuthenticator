9d2ad5e65b49f6fa3c67ab2826a3012b68d19eb0
Adam Humpherys <adam@adamh.us>
2020-01-08 08:25:36 -0700

    removing unused accountSecretKey field

--- Google.Authenticator/SetupCode.cs
+++ Google.Authenticator/SetupCode.cs
@@ -8,7 +8,6 @@ namespace Google.Authenticator
     public class SetupCode
     {
         public string Account { get; internal set; }
-        public string AccountSecretKey { get; internal set; }
         public string ManualEntryKey { get; internal set; }
         /// <summary>
         /// Base64-encoded PNG image
