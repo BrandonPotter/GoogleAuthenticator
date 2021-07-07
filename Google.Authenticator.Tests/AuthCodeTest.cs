b5ea257ce7b646394cb418c4730cc1e216a62218
Caterina Novak <caterina.novak@gmail.com>
2021-07-08 00:20:30 +0300

    Issue #65  ValidateTwoFactorPin always returns false, if the secretKey parameter is base32 encoded string - fixed
    Fix for issue # 65;
    Support for .net5;
    Small refactoring.

--- Google.Authenticator.Tests/AuthCodeTest.cs
+++ Google.Authenticator.Tests/AuthCodeTest.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using Shouldly;
 using Xunit;
-using Shouldly;
 
 namespace Google.Authenticator.Tests
 {
@@ -9,17 +8,18 @@ namespace Google.Authenticator.Tests
         [Fact]
         public void BasicAuthCodeTest()
         {
-            string secretKey = "PJWUMZKAUUFQKJBAMD6VGJ6RULFVW4ZH";
-            string expected = "551508";
+            var secretKey = "PJWUMZKAUUFQKJBAMD6VGJ6RULFVW4ZH";
+            var expected = "551508";
 
-            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
-            
-            long currentTime = 1416643820;
+            var tfa = new TwoFactorAuthenticator();
 
-            // I actually think you are supposed to divide the time by 30 seconds? Maybe need an overload that takes a DateTime?
+            var currentTime = 1416643820;
+
+            // I actually think you are supposed to divide the time by 30 seconds?
+            // Maybe need an overload that takes a DateTime?
             var actual = tfa.GeneratePINAtInterval(secretKey, currentTime, 6);
 
-            actual.ShouldBe(expected);   
+            actual.ShouldBe(expected);
         }
     }
 }
--- Google.Authenticator/SetupCode.cs
+++ Google.Authenticator/SetupCode.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Google.Authenticator
+﻿namespace Google.Authenticator
 {
     public class SetupCode
     {
