1f480743a0e860f9c2a0314995b7023019e5c0bd
Adam <adam@adamh.us>
2023-05-16 01:22:09 -0600

    ported web sample to .NET 6 MVC (#156)

---
+++ GoogleAuthenticator.WebCoreSample/Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Google.Authenticator;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace GoogleAuthenticator.WebCoreSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string QRImageUrl;
+        public string Key;
+        public string ManualEntryKey;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public void OnGet()
+        {
+            byte[] key = new byte[6];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(key);
+            Key = Convert.ToBase64String(key);
+
+            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+            SetupCode setupInfo = tfa.GenerateSetupCode("我 & You", "user@example.com", Key, false, 3);
+            QRImageUrl = setupInfo.QrCodeSetupImageUrl;
+            ManualEntryKey = setupInfo.ManualEntryKey;
+        }
+
+        public void OnPost()
+        {
+            Key = Request.Form["Key"];
+            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+            bool result = tfa.ValidateTwoFactorPIN(Key, Request.Form["txtCode"]);
+
+            SetupCode setupInfo = tfa.GenerateSetupCode("我 & You", "user@example.com", Key, false, 3);
+            QRImageUrl = setupInfo.QrCodeSetupImageUrl;
+            ManualEntryKey = setupInfo.ManualEntryKey;
+
+            TempData["ValidationResult"] = $"{Request.Form["txtCode"]} is {(result ? "" : "not ")}a valid PIN at UTC time {DateTime.UtcNow.ToString()}";
+        }
+    }
+}
