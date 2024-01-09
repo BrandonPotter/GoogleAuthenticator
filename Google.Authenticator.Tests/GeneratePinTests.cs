551557578fde55e0c246d7c18866b7df823bd1e7
Adam Humpherys <adam@adamh.us>
2024-01-09 14:26:14 -0700

    tests for algorithm updates

--- Google.Authenticator.Tests/GeneratePinTests.cs
+++ Google.Authenticator.Tests/GeneratePinTests.cs
@@ -26,6 +26,44 @@ namespace Google.Authenticator.Tests
             pinFromBytes.ShouldBe(expected);
             pinFromBase32.ShouldBe(expected);
         }
+        [Fact]
+        public void OverloadsReturnSamePINSHA256()
+        {
+            var secret = "JBSWY3DPEHPK3PXP";
+            var secretAsBytes = Encoding.UTF8.GetBytes(secret);
+            var secretAsBase32 = Base32Encoding.ToString(secretAsBytes);
+            long counter = 54615912;
+            var expected = "087141";
+
+            var subject = new TwoFactorAuthenticator(HashType.SHA256);
+
+            var pinFromString = subject.GeneratePINAtInterval(secret, counter);
+            var pinFromBytes = subject.GeneratePINAtInterval(secretAsBytes, counter);
+            var pinFromBase32 = subject.GeneratePINAtInterval(secretAsBase32, counter, secretIsBase32: true);
+
+            pinFromString.ShouldBe(expected);
+            pinFromBytes.ShouldBe(expected);
+            pinFromBase32.ShouldBe(expected);
+        }
+        [Fact]
+        public void OverloadsReturnSamePINSHA512()
+        {
+            var secret = "JBSWY3DPEHPK3PXP";
+            var secretAsBytes = Encoding.UTF8.GetBytes(secret);
+            var secretAsBase32 = Base32Encoding.ToString(secretAsBytes);
+            long counter = 54615912;
+            var expected = "397230";
+
+            var subject = new TwoFactorAuthenticator(HashType.SHA512);
+
+            var pinFromString = subject.GeneratePINAtInterval(secret, counter);
+            var pinFromBytes = subject.GeneratePINAtInterval(secretAsBytes, counter);
+            var pinFromBase32 = subject.GeneratePINAtInterval(secretAsBase32, counter, secretIsBase32: true);
+
+            pinFromString.ShouldBe(expected);
+            pinFromBytes.ShouldBe(expected);
+            pinFromBase32.ShouldBe(expected);
+        }
     }
 }
 
